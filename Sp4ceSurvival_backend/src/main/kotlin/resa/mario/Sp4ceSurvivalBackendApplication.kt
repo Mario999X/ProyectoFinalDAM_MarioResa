59d473d80edabf7ef70b35f9d3048d53d9da206c
Mario <mariogr901@gmail.com>
2023-04-05 19:53:04 +0200

    Properties básico configurado, schema y docker-compose de desarrollo configurados

--- Sp4ceSurvival_backend/src/main/kotlin/resa/mario/Sp4ceSurvivalBackendApplication.kt
+++ Sp4ceSurvival_backend/src/main/kotlin/resa/mario/Sp4ceSurvivalBackendApplication.kt
@@ -2,8 +2,12 @@ package resa.mario
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
+@EnableR2dbcRepositories
+@EnableCaching
 class Sp4ceSurvivalBackendApplication
 
 fun main(args: Array<String>) {
---
+++ Sp4ceSurvival_backend/src/main/kotlin/resa/mario/config/APIConfig.kt
@@ -0,0 +1,18 @@
+package resa.mario.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class APIConfig {
+    companion object {
+        const val API_PATH = "/sp4ceSurvival"
+
+        const val API_VERSION = "1.0.0"
+
+        const val PAGINATION_INIT = "0"
+
+        const val PAGINATION_SIZE = "10"
+
+        const val PAGINATION_SORT = "scoreNumber"
+    }
+}
